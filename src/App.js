4b78f467faa6c79ea0f03c482a26267979e82e7e
GiorgiKalatozishvili <giorgi.kalatozishvili.1@btu.edu.ge>
2022-09-03 19:52:57 +0400

    modal

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React, { useState } from "react";
+import Modal from "./components/modal";
 
 function App() {
-  return <div className="App"></div>;
+  const [openModal, setOpenModal] = useState(false);
+  return (
+    <div className="App">
+      <h1>Hi, click on the button to open the modal.</h1>
+      <button className="openModalBtn" onClick={() => setOpenModal(!openModal)}>
+        Open
+      </button>
+      {openModal && <Modal setOpenModal={setOpenModal} />}
+    </div>
+  );
 }
 
 export default App;
--- src/components/modal/index.js
+++ src/components/modal/index.js
@@ -1,8 +1,29 @@
 import React from "react";
 import "./Modal.css";
 
-const Modal = () => {
-  return <div>Modal</div>;
+const Modal = ({ setOpenModal }) => {
+  return (
+    <div className="modalBackground">
+      <div className="modalContainer">
+        <div className="titleCloseBtn">
+          <button onClick={() => setOpenModal(false)}> X </button>
+        </div>
+        <div className="title">
+          <h1>Continue?</h1>
+        </div>
+        <div className="body">
+          <p>
+            The next page is awesome! You should move forward, you will enjoy
+            it!
+          </p>
+        </div>
+        <div className="footer">
+          <button onClick={() => setOpenModal(false)}>Cancel</button>
+          <button>Continue</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Modal;
