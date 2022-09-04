c96cd87a7ddfd16d34709d52570d612d8a436da3
GiorgiKalatozishvili <giorgi.kalatozishvili.1@btu.edu.ge>
2022-09-04 10:15:53 +0400

    dropdown

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
 import React, { useState } from "react";
+import Dropdown from "./components/dropdown/Dropdown";
 import Modal from "./components/modal";
+
+const items = [
+  {
+    id: 1,
+    value: "Pulp Fiction",
+  },
+  {
+    id: 2,
+    value: "The Prestige",
+  },
+  {
+    id: 3,
+    value: "Blade Runner 2049",
+  },
+];
 
 function App() {
   const [openModal, setOpenModal] = useState(false);
@@ -10,6 +26,7 @@ function App() {
         Open
       </button>
       {openModal && <Modal setOpenModal={setOpenModal} />}
+      <Dropdown title="Select movie" items={items} />
     </div>
   );
 }
---
+++ src/components/dropdown/Dropdown.js
@@ -0,0 +1,63 @@
+import React, { useState } from "react";
+import "./Dropdown.css";
+
+const Dropdown = ({ title, items = [], multiSelect = false }) => {
+  const [open, setOpen] = useState(false);
+  const [selection, setSelection] = useState([]);
+
+  const toggle = () => setOpen(!open);
+
+  const handleOnClick = (item) => {
+    if (!selection.some((current) => current.id === item.id)) {
+      if (!multiSelect) {
+        setSelection([item]);
+      } else if (multiSelect) {
+        setSelection([...selection, item]);
+      }
+    } else {
+      let selectionAfterRemoval = selection;
+      selectionAfterRemoval = selection.filter(
+        (current) => current.id !== item.id
+      );
+      setSelection([...selectionAfterRemoval]);
+    }
+  };
+
+  const isItemInSelection = (item) => {
+    if (selection.find((current) => current.id === item.id)) {
+      return true;
+    }
+  };
+  return (
+    <div className="dd-wrapper">
+      <div
+        className="dd-header"
+        tabIndex={0}
+        role="button"
+        onKeyPress={() => toggle(!open)}
+        onClick={() => toggle(!open)}
+      >
+        <div className="dd-header__title">
+          <p className="dd-header__title--bold">{title}</p>
+        </div>
+        <div className="dd-header__action">
+          <p>{open ? "Close" : "Open"}</p>
+        </div>
+      </div>
+      {open && (
+        <ul className="dd-list">
+          {items?.map((item) => (
+            <li className="dd-list-item" key={item.id}>
+              <button type="button" onClick={() => handleOnClick(item)}>
+                <span>{item.value}</span>
+                <span>{isItemInSelection(item) && "Selected"}</span>
+              </button>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
+
+export default Dropdown;
